e7eb4edde2304906cffbb6a0270e71c2dbbe4591
joseap <josxe89@gmail.com>
2022-12-08 10:19:55 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use serde_json::{Result, Value};
+use std::fs::File;
+use std::io::prelude::*;
+use clap::Parser;
+use std::path::Path;
+use base64::{encode, decode};
+
+/// Program to convert base64 files
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Complete path to the file data
+    #[clap(short, long)]
+    path: String,
+
+    /// Complete path where the file output should be created
+    #[clap(short, long, default_value = "./output")]
+    output: String,
+}
+
+
+fn main() {
+    let args = Args::parse();
+    let path = Path::new(&args.path);
+    let output = Path::new(&args.output);
+    let mut contents= String::new();
+    let mut file_in = File::open(&path).expect("Error opening File");
+    file_in.read_to_string(&mut contents).expect("Unable to read to string");
+    let mut file_out = File::create(output).expect("Error when creating the output file");
+    if path.extension().expect("Error getting the extention") == "json" {
+        let val: Value = serde_json::from_str(&contents).expect("Error deserializing");
+        let mut data_b64 = val["dato"].as_str().unwrap(); // contains the base64 data
+        if data_b64.is_empty() {
+            data_b64 = val["archivo"][0].as_str().unwrap(); // contains the base64 data
+        }
+        let data_decoded = &decode(data_b64.as_bytes()).expect("There was an error converting the data to bytes");
+        file_out.write_all(data_decoded).expect("Error when writing to output file");
+
+
+    } else {
+        let data_decoded = &decode(contents.as_bytes()).expect("There was an error converting the data to bytes");
+        file_out.write_all(data_decoded).expect("Error when writing to output file");
+
+    }
+    println!("Program terminated successfully.");
+}
